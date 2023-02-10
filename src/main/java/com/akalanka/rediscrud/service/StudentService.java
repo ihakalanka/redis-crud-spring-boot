3e9bdcef2b6effef8aaf6d9c1dfde85b404082aa
ihakalanka <akalankaih19@gmail.com>
2023-02-10 16:04:02 +0530

    created save student service

---
+++ src/main/java/com/akalanka/rediscrud/service/StudentService.java
@@ -0,0 +1,18 @@
+package com.akalanka.rediscrud.service;
+
+import com.akalanka.rediscrud.entity.Student;
+import com.akalanka.rediscrud.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public Student saveStudent(Student student){
+        return studentRepository.save(student);
+    }
+
+
+}
