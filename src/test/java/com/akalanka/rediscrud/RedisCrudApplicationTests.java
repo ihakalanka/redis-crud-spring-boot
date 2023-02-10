94470a2f9416ea7c33466f5a99f93fe7e5fd0ad0
ihakalanka <akalankaih19@gmail.com>
2023-02-10 15:40:29 +0530

    initial commit

---
+++ src/test/java/com/akalanka/rediscrud/RedisCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akalanka.rediscrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
