f3d472703327c7ff612082cb9dbdea7ed21bb8e3
ihakalanka <akalankaih19@gmail.com>
2023-02-10 16:07:07 +0530

    created deleteStudent service

--- src/main/java/com/akalanka/rediscrud/service/StudentService.java
+++ src/main/java/com/akalanka/rediscrud/service/StudentService.java
@@ -3,6 +3,7 @@ package com.akalanka.rediscrud.service;
 import com.akalanka.rediscrud.entity.Student;
 import com.akalanka.rediscrud.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,5 +21,10 @@ public class StudentService {
 
     public Iterable<Student> getAllStudents(){
         return studentRepository.findAll();
+    }
+
+    public ResponseEntity<String> deleteStudentById(Long id){
+        studentRepository.deleteById(id);
+        return ResponseEntity.ok("Student Deleted");
     }
 }
